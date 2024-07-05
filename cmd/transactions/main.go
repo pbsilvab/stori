c7aa44ac8c5e72ad4c8d7e93cd4886ff7b4af628
Pedro <pedrosilva@aivo.co>
2024-07-05 00:11:13 -0300

    Abstraction layer for Data Storage. Implemented Inject Repositories. DynamoRepository

--- cmd/transactions/main.go
+++ cmd/transactions/main.go
@@ -8,9 +8,13 @@ import (
 	"stori-challenge/internal/account"
 	"stori-challenge/internal/emailtemplate"
 	"stori-challenge/internal/fileprocessor"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+
 	if len(os.Args) < 3 {
 		fmt.Println("Usage: main <account_id> <name> <directory>")
 		os.Exit(1)
@@ -37,18 +41,25 @@ func main() {
 		transactions = append(transactions, transaction)
 	}
 
-	acc := account.NewAccount(accountID, transactions)
+	repoType := os.Getenv("REPOSITORY_TYPE")
+	r, err := account.NewAccountRepository(repoType)
+
+	if err != nil {
+		log.Fatalf("Error getting latest CSV file: %v", err)
+	}
+
+	acc := account.NewAccount(accountID, transactions, r)
 	totalBalance, transactionsByMonth, averageCreditByMonth, averageDebitByMonth := acc.CalculateSummary()
 
 	fmt.Printf("Account ID: %s\n", acc.ID)
 	fmt.Printf("Balance: %.2f\n", totalBalance)
 
+	//TODO: if saving is ok! Defer delete file
+	acc.SaveTransactions()
+
 	eth := emailtemplate.NewEmailTemplateHandler()
-
 	summaryContent := eth.GenerateSummaryContent(totalBalance, transactionsByMonth, averageCreditByMonth, averageDebitByMonth)
-
 	template := eth.GetDefaultTemplate()
-
 	params := map[string]string{
 		"Name":         name,
 		"TotalBalance": summaryContent,
